4fb92a1137e1279a87ce488655b56e56d8e4beae
Mike Ralphson <mike.ralphson@gmail.com>
2018-03-10 18:20:50 +0000

    testRunner; omit more undefineds

--- testRunner.js
+++ testRunner.js
@@ -123,6 +123,7 @@ function* check(file) {
                 result = result.split('"undefined":').join('x');
                 result = result.split('undefinedfault').join('x');
                 result = result.split('|undefined|[Empty]').join('x');
+                result = result.split('efault: undefined').join('x');
                 if (ok && result.indexOf('undefined')>=0) {
                     message = 'Ok except for undefined references';
                     ok = false;
