1306fe0085b310fb9012839c1069e535734d804c
Linda Robledo <lrobledo@cmsonline.com>
2019-03-05 20:57:04 -0700

    create one row for each property

--- test/common.test.js
+++ test/common.test.js
@@ -5,43 +5,66 @@ const common = require('../lib/common');
 
 const blank = { title: '', rows: [], description: undefined };
 
+const schema0 = {};
+const schema1 = {
+    type: 'string',
+    description: ''
+};
+const schema4 = {
+    type: 'object',
+    description: '',
+    properties:
+        {
+            id:
+                {
+                    type: 'string'
+                },
+            data: {
+                type: 'object',
+                properties: {
+                    name: {
+                        type: 'string'
+                    },
+                    number: {
+                        type: 'string'
+                    }
+                }
+            }
+        }
+};
+
 describe('common tests', () => {
     describe('schemaToArray tests', () => {
-
         it('should return a blank container if all inputs are blank', () => {
             const schema = {};
             const offset = '';
             const options = {};
             const data = {
-                "translations": {
-                    "indent": {
-                        "repeat": () => {}
+                'translations': {
+                    'indent': {
+                        'repeat': () => {
+                        }
                     }
                 }
             };
-            assert.equal(common.schemaToArray(schema, offset, options, data)[0].title, '');
-            assert.equal(common.schemaToArray(schema, offset, options, data)[0].rows[0], undefined);
-            assert.equal(common.schemaToArray(schema, offset, options, data)[0].description, undefined);
+            assert.equal(common.schemaToArray(schema0, offset, options, data)[ 0 ].title, '');
+            assert.equal(common.schemaToArray(schema0, offset, options, data)[ 0 ].rows[ 0 ], undefined);
+            assert.equal(common.schemaToArray(schema0, offset, options, data)[ 0 ].description, undefined);
         });
 
-        it('should properly offset values', () => {
-            const schema = {
-                    "openapi": "3.0.0",
-                    "info": {
-                        "version": "",
-                        "title": "Widdershins API Example"
-                    }
-                };
+        it('should create a row for each property', () => {
             const offset = 1;
             const options = {};
             const data = {
-                "translations": {
-                    "indent": {
-                        "repeat": () => {}
+                'translations': {
+                    'indent': {
+                        'repeat': () => {
+                        }
                     }
                 }
             };
-            assert.equal(common.schemaToArray(schema, offset, options, data)[0].title, '');
+            assert.equal(common.schemaToArray(schema1, offset, options, data)[ 0 ].rows.length, 1);
+            assert.equal(common.schemaToArray(schema4, offset, options, data)[ 0 ].rows.length, 4);
         });
     });
 });
