d1c001b302e44877df883b1148222d45413c2365
Linda Robledo <lrobledo@cmsonline.com>
2020-03-03 14:13:10 -0700

    start of tagGroup and toc unit tests

--- test/openapi3.test.js
+++ test/openapi3.test.js
@@ -1,7 +1,10 @@
 'use strict';
-
 const assert = require('assert');
 const openapi3 = require('../lib/openapi3');
+const exampleEnv = require('../example_env.json');
+const oasSpec = require('./testData/oas3Petstore.json');
+const data = require('./testData/data.json');
+
 const options =
     {
         'omitBody': true,
@@ -75,6 +78,38 @@ describe('openapi3 tests', () => {
             openapi3.fakeBodyParameter(goodData);
             assert(goodData.parameters.length === 1);
         });
+    });
 
+    describe('getTagGroup', () => {
+        let test = null;
+        it('should handle no tag groups', () => {
+            assert.doesNotThrow(() => openapi3.getTagGroup('fake tag', null));
+        });
+        it('should handle wrong tags ', () => {
+            assert.doesNotThrow(() => openapi3.getTagGroup('fake tag', exampleEnv.tagGroups));
+            let result = openapi3.getTagGroup('fake tag', exampleEnv.tagGroups);
+            assert(result.name === 'fake tag');
+            assert(result.description === '');
+        });
+        it('should get the proper title & description from the tag group', () => {
+            let result = openapi3.getTagGroup('invoice-create', exampleEnv.tagGroups);
+            assert(result.name === 'Billing');
+            assert(result.description === 'billing apis');
+        });
+    });
+
+    describe('convertToToc', () => {
+        let resources = openapi3.convertToToc(oasSpec, data);
+        it('should return a pets object with count, methods and description', () => {
+            assert(resources.pets);
+            assert(resources.pets.count);
+            assert(resources.pets.methods);
+            assert(resources.pets.description === '');
+        });
+        it('methods object should include `listPets`, `createPets`, and `showPetById`', () => {
+            assert(resources.pets.methods.listPets);
+            assert(resources.pets.methods.createPets);
+            assert(resources.pets.methods.showPetById);
+        });
     });
 });
